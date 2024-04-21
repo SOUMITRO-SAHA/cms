46fffe6c01802d95cbbe92810067c6d5ef39eea7
SOUMITRA SAHA <soumitrosahaofficial@gmail.com>
2024-04-21 18:35:45 +0530

    FIX: On clicking View content it doesn't open

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -8,20 +8,13 @@ export const Courses = ({ courses }: { courses: Course[] }) => {
   const router = useRouter();
   return (
     <section className="flex flex-col items-center w-full">
-      <div className="max-w-screen-xl w-full mx-auto py-6 px-6 cursor-pointer grid grid-cols-1 gap-5 md:grid-cols-3 sm:grid-cols-2">
+      <div className="grid w-full max-w-screen-xl grid-cols-1 gap-5 px-6 py-6 mx-auto cursor-pointer md:grid-cols-3 sm:grid-cols-2">
         {courses?.map((course) => (
           <CourseCard
             key={course.id}
             course={course}
             onClick={() => {
-              if (
-                course.title.includes('Machine Learning') ||
-                course.title.includes('Harnoor')
-              ) {
-                router.push('https://harkirat.classx.co.in/');
-              } else {
-                router.push(`/courses/${course.id}`);
-              }
+              router.push(`/courses/${course.id}`);
             }}
           />
         ))}
